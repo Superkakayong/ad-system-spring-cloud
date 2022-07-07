0154754e5ff057976e768d04592a2343150bec0b
Dayong Wu <dayong@bu.edu>
2022-07-07 12:30:15 -0400

    feature - finished developing Zuul gateway

---
+++ ad-gateway/src/main/java/com/dayong/ad/ZuulGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.dayong.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication // not SpringBootApplication because we need SpringCloud to implement the gateway!
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
